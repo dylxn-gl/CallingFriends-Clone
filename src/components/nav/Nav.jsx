c73eb8f44cb0bbb920623bab381141aa5801602b
Dylan Alexander <dylanp.arango12@gmail.com>
2023-09-27 14:30:25 -0500

    Added: Navbar Responsive

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,13 +1,15 @@
+import { useState } from "react";
 import ProfilePage from "../../pages/Profile";
 import ProfileFinancesPage from "../../pages/ProfileFinances";
 import ProfileBetsPage from "../../pages/ProfileBets";
 import BetsPage from "../../pages/Bets";
 import Undefined from "../undefined/Undefined";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import { AppBar, Toolbar, Typography, Box } from "@mui/material";
+import { AppBar, Toolbar, Typography, Box, List, ListItem, ListItemIcon, ListItemText, Drawer, IconButton } from "@mui/material";
 import PaidIcon from "@mui/icons-material/Paid";
 import ExitToAppIcon from "@mui/icons-material/ExitToApp";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import MenuIcon from '@mui/icons-material/Menu';
 import { styled } from "@mui/system";
 
 //Enlaces de la navegacion
@@ -23,6 +25,7 @@ const Img = styled("img")({
 });
 
 const Nav = () => {
+  const [open, setOpen] = useState(false)
   //Navegacion principal
   return (
     <BrowserRouter>
@@ -36,7 +39,7 @@ const Nav = () => {
           <Img src="https://callingfriends.com/assets/main.webp" />
           <Box
             sx={{
-              display: "flex",
+              display: {xs: "none", md: "flex"},
             }}
           >
             {Links.map((link, index) => (
@@ -60,11 +63,20 @@ const Nav = () => {
               </Link>
             ))}
           </Box>
+          <IconButton sx={{
+              display: {xs: "block", md: "none"},
+              color: "#ffffff",
+            }} onClick={() => setOpen(true)}>
+            <MenuIcon />
+          </IconButton>
         </Toolbar>
       </AppBar>
+      <Drawer open={open} anchor="left" onClose={() => setOpen(false)}>
+        <ResponsiveNav />
+      </Drawer>
       {/*Rutas de la navegacion*/}
       <Routes>
-        <Route index path="/" element={<Box sx={{height: "90vh",}}><Undefined /></Box>}/>
+        <Route index path="/" element={<Box sx={{ height: "90vh", }}><Undefined /></Box>} />
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/profile/bets" element={<ProfileBetsPage />} />
         <Route path="/profile/payments" element={<ProfileFinancesPage />} />
@@ -75,3 +87,42 @@ const Nav = () => {
 };
 
 export default Nav;
+
+const ResponsiveNav = () => {
+  //Navegacion responsive
+  return (
+    <>
+      <Box>
+        <nav>
+          <Box sx={{
+            bgcolor: "#169ad7",
+          }}>
+            <Img src="https://callingfriends.com/assets/main.webp" width="250px" height="auto"/>
+          </Box>
+          <List>
+            {Links.map((link, index) => (
+              <Link key={index} to={link.href} style={{ textDecoration: 'none' }}>
+                <ListItem disablePadding
+                  href=""
+                  sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    bgcolor: "transparent",
+                    color: "#000000",
+                    textDecoration: "none",
+                    fontWeight: "bold",
+                  }}
+                >
+                  <ListItemIcon sx={{color: "#000000",}}>
+                    {link.icon}
+                  </ListItemIcon>
+                  <ListItemText primary={link.text} />
+                </ListItem>
+              </Link>
+            ))}
+          </List>
+        </nav>
+      </Box>
+    </>
+  )
+}
